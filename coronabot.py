8a300c67c30a5cd81e5f504bfd5be4db53bf725f
Trolliver Pust <trolliverpust@gmail.com>
2020-10-20 11:55:34 +0200

    Add update info to messages

--- coronabot.py
+++ coronabot.py
@@ -12,7 +12,7 @@ firststart = True
 
 #---- Start Telegram Part
 bot_token = # YOUR BOT TOKEN HERE
-chat_id = # YOUR CHAT ID HERE
+chat_id =# YOUR CHAT ID HERE
 
 def send_telegram(message):
     params = {"chat_id": chat_id, "text": message, "parse_mode":"markdown"}
@@ -30,6 +30,7 @@ def voice_output(text):
 #---- End Voiceoutput Part
 
 #---- Start Dic-search Part
+
 def find_county(county, data):
     for c in data["features"]:
         if c["properties"]["county"]==county:
@@ -70,6 +71,7 @@ def data2text(county,data):
     message += "Fälle insgesamt: "+str(countydic["cases"]) + "\n"
     message += "7-Tage-Inzidenz: "+str(round(countydic["cases7_per_100k"],1)) + "\n"
     message += "Todeszahlen insgesamt: "+str(countydic["deaths"]) + "\n"
+    message += "Stand: "+countydic["last_update"] + "\n"
     #message += "Alle Angaben wie immer ohne Gewähr. Quelle: https://experience.arcgis.com/experience/478220a4c454480e823b17327b2bf1d4"
     return message
 
@@ -88,7 +90,7 @@ def data2textandvoice(county,data):
     countydic = find_county(county, data)
     
     message = "*" + countydic["BEZ"] + " " + countydic['GEN'] + ": * \n"
-    
+
     voiceline = "Hier sind die aktuellen COVID-19-Zahlen für "+countydic["BEZ"] + " " + countydic['GEN']+":"
     
     if not firststart: # Comparison of cases to previous day only possible if it is not the first day
@@ -108,11 +110,12 @@ def data2textandvoice(county,data):
     voiceline += "Die Siebentageinzidenz liegt bei "+str(int(countydic["cases7_per_100k"]))+"Komma " + str(int((round(countydic["cases7_per_100k"],1)-(int(countydic["cases7_per_100k"])))*10))+ " ."
     
     message += "Todeszahlen insgesamt: "+str(countydic["deaths"]) + "\n"
+    message += "Stand: "+countydic["last_update"] + "\n"
     
     if str(countydic["deaths"]) == "1":
         voiceline += "Insgesamt gab es genau einen Todesfall"
     elif str(countydic["deaths"]) == "0":
-        voiceline += "Insgesamt gab es gar keinen Todesfall"
+        voiceline += "Insgesamt gab es keinen einzigen Todesfall"
     else:
         voiceline += "Insgesamt gab es"+str(countydic["deaths"]) + " Todesfälle"
     return [message,voiceline]
@@ -130,7 +133,6 @@ def send_text_and_voice(county,data):
 
 messagetime = 12 # Anything between 0 and 22 is valid
 
-
 done_for_today = False
 
 while True:
@@ -141,10 +143,10 @@ while True:
         data_from_today = get_data()
         
         # Replace this with the countys relevant to you
-        #send_text("SK Kaiserslautern",data_from_today)
-        send_text_and_voice("SK Kaiserslautern",data_from_today)
-        #send_text("LK Kaiserslautern",data_from_today)
-        send_text_and_voice("LK Kaiserslautern",data_from_today)
+        send_text("SK Kaiserslautern",data_from_today)
+        #send_text_and_voice("SK Kaiserslautern",data_from_today)
+        send_text("LK Kaiserslautern",data_from_today)
+        #send_text_and_voice("LK Kaiserslautern",data_from_today)
         
         firststart = False
         
